176ff837ab9dcccd6de399abb3212001ef7fe524
Mauro Carabotti <carabotti@puzzle.ch>
2013-11-22 14:48:54 +0100

    basic layout and function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,20 @@
 'use strict';
 
-angular.module('protoKoolApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('protoKoolApp').controller('MainCtrl', function ($scope) {
+
+	$scope.computerCount = 12;
+
+	$scope.computerCountChange = function() {
+		var list = [];
+
+		for (var i=1; i<=$scope.computerCount; i++) {
+			list.push(i);
+		}
+
+		$scope.computers = list;
+	};
+
+	$scope.computerCountChange();
+
+	$scope.others =['Telefon', 'Anderes', 'Papier', 'Break/Missing'];
+});
